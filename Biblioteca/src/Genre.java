1c01145cabc074aaf00f26229e35775df10bb670
CarlosDeMoraGil <146753433+CarlosDeMoraGil@users.noreply.github.com>
2023-10-26 15:31:45 +0200

    Actualizacion ejercicio Biblioteca

---
+++ Biblioteca/src/Genre.java
@@ -0,0 +1,14 @@
+public class Genre {
+
+    private String name;
+
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+    public String getDescription(){
+        return description;
+    }
+
+}
---
+++ Biblioteca/src/Loan.java
@@ -0,0 +1,11 @@
+public class Loan {
+
+    private String id;
+    private String date;
+
+    public Users users;
+    public Book book;
+
+
+
+}
