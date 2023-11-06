9033d03c8e5f26041f9e535c466308f1e2189dff
CarlosDeMoraGil <146753433+CarlosDeMoraGil@users.noreply.github.com>
2023-11-06 17:55:19 +0100

    Actualizacion Main

--- Biblioteca/src/Main.java
+++ Biblioteca/src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+    Genre genre = new Genre();
+        genre.setCode("1");
+        genre.setName("Comedia");
+        genre.setDescription("Genero de xxxxx");
+
+        System.out.print("Codigo del genero " + genre.getName() + " " + genre.getCode());
 
     }
 }
